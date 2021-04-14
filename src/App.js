86e9aad1a8cbd2a7bb31e4a2d637c9026cc45149
Joan Currie <jecurrie8@gmail.com>
2021-04-14 16:17:24 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+//import ReactDOM from "react-dom";
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+//ReactDOM.render(<h1>Hello world</h1>, document.getElementById("app"));
+export default App;
